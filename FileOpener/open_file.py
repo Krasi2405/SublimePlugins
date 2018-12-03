97bfa0b017ff6d35c3b528cca7aea12d85c7d614
Krasi2405 <krasi2405@gmail.com>
2018-12-03 21:42:47 +0200

    Initial. Barely working. Output from executable files not yet shown

---
+++ FileOpener/open_file.py
@@ -0,0 +1,77 @@
+import sublime
+import sublime_plugin
+import os
+import json
+
+class Command:
+
+	def __init__(self, command, executable_command = ""):
+		self.command = command
+		self.executable_command = executable_command
+		self.is_executable = (executable_command != "")
+
+	def execute_command(self, abs_path):
+		directory = os.path.split(abs_path)[0]
+		directory += "/"
+
+		filename = os.path.split(abs_path)[1]
+
+		parse_dict = {
+			"<file_name>": filename,
+			"<dir>": directory
+		}
+
+		command = self.format(self.command, parse_dict)
+		os.system(command)
+		if self.is_executable:
+			executable_command = self.format(self.executable_command, parse_dict)
+			os.system(executable_command)
+
+	def format(self, base, parse_dict):
+		for key in parse_dict:
+			base = base.replace(key, parse_dict[key])
+		return base
+
+
+class ExtensionOpener:
+
+	def __init__(self, command_file):
+		self.commands = {}
+
+		packages = sublime.packages_path()
+		package = os.path.join(packages, "FileOpener")
+		command_file = os.path.join(package, command_file)
+
+		file = open(command_file, "r")
+		data = file.read()
+		command_list = json.loads(data)
+
+		for command in command_list:
+			executable = ""
+			if "executable_command" in command:
+				executable = command["executable_command"]
+
+			cmd = Command(command["command"], executable)
+			self.commands[command["extension"]] = cmd
+
+
+
+	def execute_command(self, extension, abs_path):
+		if extension not in self.commands:
+			print("{} extension not found!".format(extension))
+			return
+		command = self.commands[extension]
+		command.execute_command(abs_path)
+
+
+
+
+class OpenFileCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		self.extensions = ExtensionOpener("commands.txt")
+		path = self.view.file_name()
+		filename = os.path.split(path)[1];
+		extension = filename.split(".")[1];
+		self.extensions.execute_command(extension, path)
+
